d358ab2254061cd6630a4b9f8a7206883d8dc399
White12352 <86940955+White12352@users.noreply.github.com>
2023-01-04 21:06:18 +0800

    Update diy-part2.sh

--- ramips-hnat/diy-part2.sh
+++ ramips-hnat/diy-part2.sh
@@ -33,35 +33,24 @@ sed -i 's/192.168.1.1/192.168.5.5/g' package/base-files/files/bin/config_generat
 svn co https://github.com/281677160/openwrt-package/trunk/luci-app-adguardhome package/luci-app-adguardhome
 svn co https://github.com/281677160/openwrt-package/trunk/adguardhome package/adguardhome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-easymesh package/luci-app-easymesh
-rm -rf package/helloworld
+#rm -rf package/helloworld
+rm -rf feeds/luci/applications/luci-app-ssr-plus
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-rm -rf package/passwall
-#git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
-#git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-vssr
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall package/passwall
 git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 #rm -rf package/feeds/luci/luci-theme-neobird
 #git clone https://github.com/lwb1978/luci-theme-neobird.git package/feeds/luci/luci-theme-neobird
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon-18.06
-rm -rf package/luci-app-unblockneteasemusic
+rm -rf feeds/luci/applications/luci-app-unblockneteasemusic
 git clone -b master https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
-
-pushd package/emortal/
-#rm -rf lua-maxminddb
-#git clone https://github.com/jerrykuku/lua-maxminddb.git
-#rm -rf luci-app-vssr
-#git clone https://github.com/jerrykuku/luci-app-vssr.git
-rm -rf luci-app-openclash
-git clone https://github.com/vernesong/OpenClash
-# rm -rf luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-# rm -rf luci-theme-neobird
-# git clone https://github.com/lwb1978/luci-theme-neobird.git
-rm -rf luci-app-omcproxy
-git clone -b 18.06 https://github.com/lwb1978/luci-app-omcproxy.git
-rm -rf luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/destan19/OpenAppFilter.git
-popd
+rm -rf feeds/luci/applications/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-omcproxy
+git clone -b 18.06 https://github.com/lwb1978/luci-app-omcproxy.git package/luci-app-omcproxy
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # 把bootstrap替换成argon为源码必选主题（可自行修改您要的,主题名称必须对,比如下面代码的[argon],源码内必须有该主题,要不然编译失败）
 sed -i "s/bootstrap/argon-18.06/ig" feeds/luci/collections/luci/Makefile
